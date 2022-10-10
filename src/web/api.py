11887ddcd371e167a536738fd002c7824ce1a5a7
Shawn Duong <shawnduong@protonmail.com>
2022-10-09 20:31:20 -0700

    Implemented POST responses for card provisioning.

--- src/web/api.py
+++ src/web/api.py
@@ -7,5 +7,22 @@ def get_users():
 	Give the user a JSON list of users' IDs.
 	"""
 
-	return {"USERS": [u.cardID for u in User.query.all()]}
+	return {"USERS": [u.cardID for u in User.query.all()]}, 200
+
+@app.route("/api/users/create", methods=["POST"])
+def create_user():
+	"""
+	Create a new user with some cardID and 0 points.
+	"""
+
+	cardID = request.json["cardID"]
+
+	try:
+		user = User(cardID, 0)
+		db.session.add(user)
+		db.session.commit()
+	except:
+		return {"STATUS": "FAILURE"}, 500
+
+	return {"STATUS": "SUCCESS"}, 200
 
