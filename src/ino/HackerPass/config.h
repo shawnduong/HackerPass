d6884e23555795aebc9d2314a7e8f20bec4edba1
Shawn Duong <shawnduong@protonmail.com>
2022-10-15 22:23:05 -0700

    Created new HP_KEY in config.

--- src/ino/HackerPass/config.h
+++ src/ino/HackerPass/config.h
@@ -1,9 +1,12 @@
 /* Network information. */
-#define SSID     "YOUR_NETWORK_NAME_HERE"
-#define PASSWORD "YOUR_NETWORK_PASS_HERE"
+#define SSID      "YOUR_NETWORK_NAME_HERE"
+#define PASSWORD  "YOUR_NETWORK_PASS_HERE"
 
 /* How long to wait for a connection. */
-#define TIMEOUT  5000  // ms
+#define TIMEOUT   5000  // ms
 
 /* API location. Do not include a trailing slash. */
-#define API      "https://api.website.here"
+#define API       "https://api.website.here"
+
+/* Unique identification key for this particular unit. */
+#define HP_KEY    0x00000000
