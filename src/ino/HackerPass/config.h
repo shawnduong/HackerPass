585212ddfd7f5f63f176933909b42b97126267b4
Shawn Duong <shawnduong@protonmail.com>
2022-10-15 22:01:51 -0700

    Updated config format.

--- src/ino/HackerPass/config.h
+++ src/ino/HackerPass/config.h
@@ -2,8 +2,8 @@
 #define SSID     "YOUR_NETWORK_NAME_HERE"
 #define PASSWORD "YOUR_NETWORK_PASS_HERE"
 
-/* How long in ms to wait for a connection. */
-#define TIMEOUT  5000
+/* How long to wait for a connection. */
+#define TIMEOUT  5000  // ms
 
-/* API location. */
-#define API      "https://api.website.here/"
+/* API location. Do not include a trailing slash. */
+#define API      "https://api.website.here"
