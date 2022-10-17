989ff3243a5d07b03201986ccfc33d2e8209c8df
Shawn Duong <shawnduong@protonmail.com>
2022-10-16 17:31:16 -0700

    Implemented card reading and attendance POSTing.

--- src/ino/HackerPass/HackerPass.ino
+++ src/ino/HackerPass/HackerPass.ino
@@ -1,39 +1,50 @@
 #include "include/api.h"
+#include "include/rc522.h"
 #include "include/networking.h"
 #include "config.h"
 
 /* HTTP response code and response data buffers. */
 uint16_t code;
 String response;
-uint16_t counter;  // Debugging.
+
+uint64_t eventID = 1;  // Debugging.
+uint32_t id;
+byte buffer[12];
+byte len;
 
 void setup()
 {
 	Serial.begin(9600);
-	counter = 0;
+	rc522_init();
 }
 
 void loop()
 {
+	id = 0;
+
 	/* Check for a network connection. Reconnect if disconnected. */
 	if (!is_connected())
 	{
 		Serial.println("Not connected. Connecting now.");
-		Serial.print("MAC: ");
-		Serial.println(get_mac());
+		Serial.println("MAC: " + get_mac());
 
 		if (connect(SSID, PASSWORD, TIMEOUT))  Serial.println("Connection success.");
 		else                                   Serial.println("Connection failure.");
 	}
 
 	/* Debugging. */
-	post(API, "/api/hp/user/create", HP_KEY, &code, &response, "{\"cardID\":"+String(counter)+"}");
-	Serial.println(code);
-	Serial.println(response);
-	get(API, "/api/hp/user", HP_KEY, &code, &response);
-	Serial.println(code);
-	Serial.println(response);
-	counter++;
+	if (read_uid(10, buffer, &len))
+	{
+		id = (buffer[0] << 0x18) | (buffer[1] << 0x10) | (buffer[2] << 0x08) | buffer[3];
+
+		Serial.println("Card detected.");
+		Serial.println(id);
+
+		post(API, "/api/hp/attendance/create", HP_KEY, &code, &response,
+			"{\"user\":"+String(id)+",\"event\":"+String(eventID)+"}");
+		Serial.println(code);
+		Serial.println(response);
+	}
 
 	delay(1000);
 }
---
+++ src/ino/HackerPass/include/rc522.h
@@ -0,0 +1,32 @@
+#ifndef RC522_H
+#define RC522_H
+
+#include <MFRC522.h>   // MFRC522 library by GithubCommunity.
+
+MFRC522 mfrc522(4, 5);
+
+void rc522_init()
+{
+	SPI.begin();
+	mfrc522.PCD_Init();
+}
+
+bool read_uid(byte attempts, byte *buffer, byte *len)
+{
+	for (byte i = 0; i < attempts; i++)
+	{
+		if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+		{
+			for (byte j = 0; j < mfrc522.uid.size; j++)
+				buffer[j] = mfrc522.uid.uidByte[j];
+			*len = mfrc522.uid.size;
+			return true;
+		}
+
+		delay(100);
+	}
+
+	return false;
+}
+
+#endif
