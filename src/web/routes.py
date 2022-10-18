9b0398e7c8859d8c1a0792d219bf388c9d7a01de
Shawn Duong <shawnduong@protonmail.com>
2022-10-18 02:22:07 -0700

    Implemented basic user page UI.

--- src/web/routes.py
+++ src/web/routes.py
@@ -2,11 +2,15 @@ from app import *
 
 from flask_login import current_user, login_required
 
-@app.route("/", methods=["GET"])
+@app.route("/"     , methods=["GET"])
+@app.route("/login", methods=["GET"])
 def index():
 	"""
 	Display the index page to the user.
 	"""
+
+	if current_user.is_authenticated:
+		return redirect(url_for("application"))
 
 	return render_template("index.html")
 
